0952335d9d01c64e6a3fdeee3768a55d0fd8b1cf
huker <huaolie@sina.com>
2016-05-09 12:39:36 +0800

    数据库连接，添加、列出实现

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var todoModel = require('../model');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+
+router.get('/todo', function (req, res, next) {
+    todoModel.find({}, function (err, data) {
+        if (err) {
+            res.send({code:0,msg:'查询错误'})
+        } else {
+            res.send(data);
+        }
+
+    })
+});
+
+router.post('/todo', function (req, res, next) {
+    todoModel.create(req.body, function (err, data) {
+        if(err){
+            res.send({code:0,msg:'添加错误'})
+        }else{
+            res.send(data)
+        }
+    })
 });
 
 module.exports = router;
