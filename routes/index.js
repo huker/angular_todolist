96d9c4fe54edcee485958fc48961ef52700c0cc8
huker <huaolie@sina.com>
2016-05-09 23:08:16 +0800

    单个删除

--- routes/index.js
+++ routes/index.js
@@ -28,4 +28,14 @@ router.post('/todo', function (req, res, next) {
     })
 });
 
+router.post('/todo/:_id',function(req,res,next){
+    todoModel.remove({_id:req.params._id},function(err,result){
+        if(err){
+            res.send({code:0,msg:'删除错误'})
+        }else{
+            res.send({code:1,msg:'删除成功'})
+        }
+    })
+});
+
 module.exports = router;
